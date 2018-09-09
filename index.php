57e9f7490ad6ec1280e5cce38726dd08eadeed76
Jun15972 <jun4096@gmail.com>
2018-09-09 17:21:07 +0900

    トップページを作った

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>ジュン2のホームページ オリジナルコンテンツ</title>
+	<link rel="stylesheet" type="text/css" href="sty1.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<main>
+		<h1>
+		    ジュン2のホームページ オリジナルコンテンツ</h1>
+		<p>
+		    WordPressとかを使わずに1から作ったものをorigと名付けた一つのディレクトリに押し込み、そうすることによってリポジトリにすることができてGitHubの練習もできる　という感じでやっている場所です。手始めに、既存の楽曲が一列に並ぶデータベースのページをこの中に改めて作ろうとしています。</p>
+		<nav>
+		    <p>
+		        <a href="https://github.com/Jun15972/jun2homepage-orig">GitHubでこのリポジトリを見るにはこのリンクをクリックしてください</a></p>
+		    <p>
+		        <a href="/">ジュン2のホームページのトップページへ行くには、このリンクをクリックしてください</a></p>
+		</nav>
+	</main>
+</body>
+</html>
